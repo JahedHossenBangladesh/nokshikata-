04eeb9e564dcdfc94b32c3d161956044c5b17746
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-17 01:29:50 +0600

    This is e commarce

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/dist/shared/lib/head'
+import { AppBar } from '@material-ui/core'
+function Layout() {
+    return (
+        <div>
+            <Head>
+                <title>Next Amazon</title>
+            </Head>
+            <AppBar>
+
+
+                </AppBar>
+
+        </div>
+    )
+}
+
+export default Layout
