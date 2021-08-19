3b519db413720352fc67044a89e6a7e0ba2eabc1
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-08-19 23:45:32 +0600

    Product pace is getting ready

---
+++ utils/data.js
@@ -0,0 +1,13 @@
+const data ={
+
+    products:[
+        {
+            name: 'Ash Dress',
+            category:'2 pice',
+            price: '$100',
+            brand: 'Ash',
+            rating: '4.5',
+            
+        }
+    ]
+}
